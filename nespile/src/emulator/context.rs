2e8dca54e53d59989ff14a0a967204979023d41b
Scott Fasone <scott181182@yahoo.com>
2024-07-20 22:00:16 -0400

    feat: expanded CPU and memory model

--- nespile/src/emulator/context.rs
+++ nespile/src/emulator/context.rs
@@ -52,112 +52,115 @@ pub trait EmulationContext {
 
 
 
-
+pub struct RealCpuContext {
+    pub a: u8,
+    pub x: u8,
+    pub y: u8,
+    pub sp: u8,
+    pub pc: u16,
+    pub flags: u8,
+}
 pub struct RealEmulationContext {
-    a: u8,
-    x: u8,
-    y: u8,
-    sp: u8,
-    pc: u16,
-    flags: u8,
-
-    memory: NesMemory,
+    cpu: RealCpuContext,
+    pub memory: NesMemory,
 }
 impl RealEmulationContext {
     const STACK_BOTTOM: u16 = 0x01ffu16;
 
     pub fn new(memory: NesMemory) -> Self {
         RealEmulationContext{
-            a: 0, x: 0, y: 0,
-            sp: 0,
-            pc: 0x8000,
-            flags: 0,
+            cpu: RealCpuContext{
+                a: 0, x: 0, y: 0,
+                sp: 0,
+                pc: 0x8000,
+                flags: 0,
+            },
             memory
         }
     }
 
     fn stack_address(&self) -> u16 {
-        Self::STACK_BOTTOM - self.sp as u16
+        Self::STACK_BOTTOM - self.cpu.sp as u16
     }
 }
 impl EmulationContext for RealEmulationContext {
-    fn read_reg_a(&self) -> u8 { self.a }
-    fn write_reg_a(&mut self, value: u8) -> () { self.a = value; }
+    fn read_reg_a(&self) -> u8 { self.cpu.a }
+    fn write_reg_a(&mut self, value: u8) -> () { self.cpu.a = value; }
 
-    fn read_reg_x(&self) -> u8 { self.x }
-    fn write_reg_x(&mut self, value: u8) -> () { self.x = value; }
+    fn read_reg_x(&self) -> u8 { self.cpu.x }
+    fn write_reg_x(&mut self, value: u8) -> () { self.cpu.x = value; }
 
-    fn read_reg_y(&self) -> u8 { self.y }
-    fn write_reg_y(&mut self, value: u8) -> () { self.y = value; }
+    fn read_reg_y(&self) -> u8 { self.cpu.y }
+    fn write_reg_y(&mut self, value: u8) -> () { self.cpu.y = value; }
 
-    fn read_reg_sp(&self) -> u8 { self.sp }
-    fn write_reg_sp(&mut self, value: u8) -> () { self.sp = value; }
+    fn read_reg_sp(&self) -> u8 { self.cpu.sp }
+    fn write_reg_sp(&mut self, value: u8) -> () { self.cpu.sp = value; }
 
-    fn read_reg_pc(&self) -> u16 { self.pc }
-    fn write_reg_pc(&mut self, value: u16) -> () { self.pc = value; }
+    fn read_reg_pc(&self) -> u16 { self.cpu.pc }
+    fn write_reg_pc(&mut self, value: u16) -> () { self.cpu.pc = value; }
 
     fn read_status(&self) -> u8 {
-        self.flags
+        self.cpu.flags
     }
     fn write_status(&mut self, status: u8) -> () {
-        self.flags = status;
+        self.cpu.flags = status;
     }
 
 
 
-    fn read_flag_carry(&self) -> bool { self.flags & 0x01 > 0 }
+    fn read_flag_carry(&self) -> bool { self.cpu.flags & 0x01 > 0 }
     fn write_flag_carry(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x01; }
-        else { self.flags &= !0x01; }
+        if value { self.cpu.flags |= 0x01; }
+        else { self.cpu.flags &= !0x01; }
     }
 
     fn read_flag_zero(&self) -> bool {
-        self.flags & 0x02 > 0
+        self.cpu.flags & 0x02 > 0
     }
     fn write_flag_zero(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x02; }
-        else { self.flags &= !0x02; }
+        if value { self.cpu.flags |= 0x02; }
+        else { self.cpu.flags &= !0x02; }
     }
 
     fn read_flag_interrupt_disable(&self) -> bool {
-        self.flags & 0x04 > 0
+        self.cpu.flags & 0x04 > 0
     }
     fn write_flag_interrupt_disable(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x04; }
-        else { self.flags &= !0x04; }
+        if value { self.cpu.flags |= 0x04; }
+        else { self.cpu.flags &= !0x04; }
     }
 
     fn read_flag_decimal_mode(&self) -> bool {
-        self.flags & 0x08 > 0
+        self.cpu.flags & 0x08 > 0
     }
     fn write_flag_decimal_mode(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x08; }
-        else { self.flags &= !0x08; }
+        if value { self.cpu.flags |= 0x08; }
+        else { self.cpu.flags &= !0x08; }
     }
 
     fn read_flag_break_command(&self) -> bool {
-        self.flags & 0x10 > 0
+        self.cpu.flags & 0x10 > 0
     }
     fn write_flag_break_command(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x10; }
-        else { self.flags &= !0x10; }
+        if value { self.cpu.flags |= 0x10; }
+        else { self.cpu.flags &= !0x10; }
     }
 
     fn read_flag_overflow(&self) -> bool {
-        self.flags & 0x40 > 0
+        self.cpu.flags & 0x40 > 0
     }
     fn write_flag_overflow(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x40; }
-        else { self.flags &= !0x40; }
+        if value { self.cpu.flags |= 0x40; }
+        else { self.cpu.flags &= !0x40; }
     }
 
     fn read_flag_negative(&self) -> bool {
-        self.flags & 0x80 > 0
+        self.cpu.flags & 0x80 > 0
     }
 
     fn write_flag_negative(&mut self, value: bool) -> () {
-        if value { self.flags |= 0x80; }
-        else { self.flags &= !0x80; }
+        if value { self.cpu.flags |= 0x80; }
+        else { self.cpu.flags &= !0x80; }
     }
 
     fn read_memory_byte(&self, address: u16) -> u8 {
@@ -175,11 +178,11 @@ impl EmulationContext for RealEmulationContext {
     
     fn push_stack_byte(&mut self, value: u8) -> () {
         self.memory.write_byte_at(self.stack_address(), value);
-        self.sp -= 1;
+        self.cpu.sp -= 1;
     }
     fn pop_stack_byte(&mut self) -> u8 {
         let res = self.memory.read_byte_at(self.stack_address());
-        self.sp += 1;
+        self.cpu.sp += 1;
         res
     }
     fn push_stack_word(&mut self, value: u16) -> () {
@@ -189,7 +192,7 @@ impl EmulationContext for RealEmulationContext {
     fn pop_stack_word(&mut self) -> u16 {
         let hb = self.pop_stack_byte();
         let lb = self.pop_stack_byte();
-        return (hb << 8) as u16 | lb as u16;
+        return ((hb as u16) << 8) | lb as u16;
     }
 
     fn interrupt(&mut self) -> () {
--- nespile/src/emulator/memory.rs
+++ nespile/src/emulator/memory.rs
@@ -1,24 +1,54 @@
+use bytes::{Bytes, BytesMut};
 
 
 
 pub struct NesMemory {
-    inner: [u8; 0x10000],
+    pub zeropage: BytesMut,
+    pub stack: BytesMut,
+    pub prgrom: Bytes
 }
 impl NesMemory {
+    pub fn new(prgrom: Bytes) -> Self {
+        NesMemory{
+            zeropage: BytesMut::zeroed(0x100),
+            stack: BytesMut::zeroed(0x100),
+            prgrom
+        }
+    }
+
+
+
     pub fn read_byte_at(&self, address: u16) -> u8 {
-        self.inner[address as usize]
+        let address = address as usize;
+        if address < 0x100 {
+            self.zeropage[address]
+        } else if address < 0x200 {
+            self.stack[address - 0x100]
+        } else if address >= 0x8000 {
+            self.prgrom[address - 0x8000]
+        } else {
+            println!("Access at {:04x}", address);
+            0
+        }
     }
     pub fn write_byte_at(&mut self, address: u16, value: u8) -> () {
-        self.inner[address as usize] = value;
+        let address = address as usize;
+        if address < 0x100 {
+            self.zeropage[address] = value;
+        } else if address < 0x200 {
+            self.stack[address - 0x100] = value;
+        } else {
+            println!("Invalid write at {:04x}", address);
+        }
     }
 
     pub fn read_word_at(&self, address: u16) -> u16 {
-        let lb = self.inner[address as usize] as u16;
-        let hb = self.inner[(address + 1) as usize] as u16;
+        let lb = self.read_byte_at(address) as u16;
+        let hb = self.read_byte_at(address + 1) as u16;
         (hb << 8) | lb
     }
     pub fn write_word_at(&mut self, address: u16, value: u16) -> () {
-        self.inner[address as usize] = (value & 0xff) as u8;
-        self.inner[(address + 1) as usize] = (value >> 8) as u8;
+        self.write_byte_at(address, (value & 0xff) as u8);
+        self.write_byte_at(address + 1, (value >> 8) as u8);
     }
 }
